4c65147b0883ee05ec6606769249c4fe0e0eb9d2
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-15 18:21:52 +0200

    fixed js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,58 @@
-const newTask = document.getElementById("newtask-form"); /* creo un riferimento al form */
+// creo un riferimento al form
+const newTask = document.getElementById("newtask-form");
 
 newTask.addEventListener("submit", (event) => {
-  event.preventDefault(); /* evito che al submit si ricarichi la pagine*/
-  const taskNameInput = document.getElementById("task-name"); /* per prenedere il valore di input prima seleziono l'elemento */
-  newTaskName = taskNameInput.value; /* assegno ad una variabile il valore di input */
+  // evito che al submit si ricarichi la pagine
+  event.preventDefault();
 
+  // per prendere il valore di input prima seleziono l'elemento
+  const taskNameInput = document.getElementById("task-name");
+
+  // assegno ad una variabile il valore di input
+  newTaskName = taskNameInput.value;
+
+  // rimuovo il p quando aggiungo un task
+  const noTasks = document.getElementById("no-tasks");
+  if (noTasks) {
+    noTasks.setAttribute("style", "display: none");
+  }
+
+  // creo un li e gli do come valore il valore di input
   const task = document.createElement("li");
   task.innerText = newTaskName;
 
-  const list = document.getElementById("task-list");
-  list.appendChild(task);
-
+  // creo uno span e ci metto dentro l'icona per eliminare i task
   const deleteBtn = document.createElement("span");
-  deleteBtn.innerText = "Delete task";
   deleteBtn.innerHTML = "<ion-icon name='trash-bin-outline'></ion-icon>";
-  list.appendChild(deleteBtn);
+  deleteBtn.classList.add("list-icon");
 
+  // creo un div e ci inserisco dentro li e span creati precedentemente creati
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task-element");
+  taskElement.append(task, deleteBtn);
+
+  // seleziono l'elemento dove inserire il div e ce lo inserisco
+  const list = document.getElementById("task-list");
+  list.appendChild(taskElement);
+
+  // puluisco l'input
   taskNameInput.value = "";
 
+  // aggiungo click listener per togglare una classe per sbarrare il testo
   task.addEventListener("click", (event) => {
     task.classList.toggle("checked-task");
   });
 
+  // aggiungo click listener per rimuovere il task alla pressione dell'icona
   deleteBtn.addEventListener("click", (event) => {
-    task.remove();
-    deleteBtn.remove();
+    taskElement.remove();
   });
-});
 
-const taskElement = document.createElement("div");
+  /* deleteBtn.addEventListener("mouseover", (event) => {
+    deleteBtn.innerHTML = "<ion-icon name='trash-bin'></ion-icon>";
+  });
+ */
+  /* deleteBtn.addEventListener("mouseleave", (event) => {
+    deleteBtn.innerHTML = "<ion-icon name='trash-bin-outline'></ion-icon>";
+  }); */
+});
